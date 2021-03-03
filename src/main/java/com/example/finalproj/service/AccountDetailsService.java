a64a3bb5cfc09b3c157ec1a70cd7727cee8a049e
Miras <sektor.miras@gmail.com>
2021-03-03 22:32:12 +0600

    added: account and role dtos, repositories; AccountDetailsService

---
+++ src/main/java/com/example/finalproj/service/AccountDetailsService.java
@@ -0,0 +1,30 @@
+package com.example.finalproj.service;
+
+import com.example.finalproj.repository.AccountRepository;
+import com.example.finalproj.repository.dto.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountDetailsService implements UserDetailsService {
+
+
+    private final AccountRepository accountRepository;
+
+    @Autowired
+    public AccountDetailsService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        Account account = accountRepository.findByUsername(s);
+        if (account == null) {
+            throw new UsernameNotFoundException("Account with such username doesn't exist");
+        }
+        return account;
+    }
+}
