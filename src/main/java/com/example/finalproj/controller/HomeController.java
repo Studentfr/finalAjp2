efe9153779f7254ad2fd922af914f2e244a9667a
Sungat Kaparov <sungatkaparov8@gmail.com>
2021-03-07 01:14:57 +0600

    Question list and answer list implemented in home page; added: QuestionService with addVote method, but it is not working now because of some hibernate errors

--- src/main/java/com/example/finalproj/controller/HomeController.java
+++ src/main/java/com/example/finalproj/controller/HomeController.java
@@ -2,7 +2,10 @@ package com.example.finalproj.controller;
 
 import com.example.finalproj.repository.RoleRepository;
 import com.example.finalproj.repository.dto.Account;
+import com.example.finalproj.repository.dto.Answer;
+import com.example.finalproj.repository.dto.Question;
 import com.example.finalproj.service.AccountDetailsService;
+import com.example.finalproj.service.QuestionService;
 import com.example.finalproj.service.RoleService;
 import com.example.finalproj.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,16 +16,20 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/")
 public class HomeController {
     private RoleService roleService;
     private AccountDetailsService accountDetailsService;
     private UserService userService;
+    private QuestionService questionService;
 
     @Autowired
-    public HomeController(RoleService roleService, AccountDetailsService accountDetailsService, UserService userService) {
+    public HomeController(RoleService roleService, AccountDetailsService accountDetailsService, UserService userService, QuestionService questionService) {
         this.roleService = roleService;
+        this.questionService = questionService;
         this.accountDetailsService = accountDetailsService;
         this.userService = userService;
     }
@@ -35,6 +42,8 @@ public class HomeController {
     @GetMapping("/home")
     public String home(Model model){
         model.addAttribute("user", accountDetailsService.getAccount());
+        model.addAttribute("question", questionService.getAllQuestions());
+        model.addAttribute("answer", questionService.getAllAnswers());
         return "home";
     }
 
@@ -49,6 +58,12 @@ public class HomeController {
     public String addUser(@ModelAttribute("user") Account account) {
         account.setRole(roleService.getRole(account.getRole().getRoleId()));
         userService.registerUser(account);
+        return "redirect:/";
+    }
+
+    @PostMapping("/home")
+    public String answer(@ModelAttribute("q")Question question, @ModelAttribute("a")Answer answer, @ModelAttribute("user")Account user){
+        questionService.setVote(question,answer,user);
         return "redirect:/";
     }
 
---
+++ src/main/java/com/example/finalproj/service/QuestionService.java
@@ -0,0 +1,45 @@
+package com.example.finalproj.service;
+
+import com.example.finalproj.repository.AnswerRepository;
+import com.example.finalproj.repository.QuestionRepository;
+import com.example.finalproj.repository.VoteRepository;
+import com.example.finalproj.repository.dto.Account;
+import com.example.finalproj.repository.dto.Answer;
+import com.example.finalproj.repository.dto.Question;
+import com.example.finalproj.repository.dto.Vote;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class QuestionService {
+    private QuestionRepository questionRepository;
+    private AnswerRepository answerRepository;
+    private VoteRepository voteRepository;
+
+    @Autowired
+    public QuestionService(QuestionRepository questionRepository, AnswerRepository answerRepository, VoteRepository voteRepository){
+        this.questionRepository = questionRepository;
+        this.voteRepository = voteRepository;
+        this.answerRepository = answerRepository;
+    }
+
+    public List<Question> getAllQuestions(){
+        return questionRepository.findAll();
+    }
+    public List<Answer> getAllAnswers(){
+        return answerRepository.findAll();
+    }
+    public void setVote(Question q, Answer a, Account u){
+        Vote vote = new Vote();
+        vote.setQuestion(q);
+        vote.setAnswer(a);
+        vote.setDate(new Timestamp(new Date().getTime()));
+        vote.setUser(u);
+        voteRepository.save(vote);
+    }
+
+}
