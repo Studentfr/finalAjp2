4adfc4e7ec6b832ee1971df4bde83082728f4f4f
Amiran Kurman <amiranfox@gmail.com>
2021-03-10 16:59:58 +0600

    added votes resource test api

--- src/main/java/com/example/finalproj/rest/VoteResource.java
+++ src/main/java/com/example/finalproj/rest/VoteResource.java
@@ -1,13 +1,19 @@
 package com.example.finalproj.rest;
 
+import com.example.finalproj.repository.AnswerRepository;
+import com.example.finalproj.repository.VoteRepository;
 import com.example.finalproj.repository.dto.Question;
+import com.example.finalproj.repository.dto.Vote;
 import com.example.finalproj.rest.dto.QuestionDto;
 import com.example.finalproj.rest.dto.QuestionWebDto;
 import com.example.finalproj.rest.dto.VoteDto;
 import com.example.finalproj.service.QuestionService;
+import com.example.finalproj.service.UserService;
 import com.example.finalproj.util.Converter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,9 +24,13 @@ import java.util.List;
 @RequestMapping("/api/votes")
 public class VoteResource {
     private QuestionService questionService;
+    private AnswerRepository answerRepository;
+    private UserService userService;
     @Autowired
-    public VoteResource(QuestionService questionService) {
+    public VoteResource(UserService userService,QuestionService questionService, AnswerRepository answerRepository) {
         this.questionService = questionService;
+        this.answerRepository = answerRepository;
+        this.userService = userService;
     }
 
     @GetMapping
@@ -36,5 +46,15 @@ public class VoteResource {
             dtoList.add(dto);
         }
         return dtoList;
+    }
+
+    @GetMapping(path = "test", consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public void testVote(@RequestBody VoteDto vote) {
+        Vote vote1 = new Vote();
+        vote1.setVoteId(vote.getVoteId());
+        vote1.setDate(vote.getDate());
+        vote1.setAnswer(answerRepository.getAnswerByAnswerId(vote.getAnswerId()));
+        vote1.setUser(userService.getUser(vote.getUserId()));
+        System.out.println(vote1);
     }
 }
